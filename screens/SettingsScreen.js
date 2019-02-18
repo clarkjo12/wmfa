f722a512277538263ce514b50bc64c6e56a2fc7a
Jon <clarkjo12@gmail.com>
2019-02-18 02:29:25 -0500

    checkin in

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import React from "react";
+import { View, Text } from "react-native";
+import styled from "styled-components";
+
+const MommaDiv = styled.View`
+  background: #38b6ff;
+  height: 100%;
+`;
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    header: null
   };
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+      <MommaDiv>
+        <Text>okoko</Text>
+      </MommaDiv>
+    );
   }
 }

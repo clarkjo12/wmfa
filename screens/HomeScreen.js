6d18f6ac84215ee496aef988072fab6006bb1b78
Jon <clarkjo12@gmail.com>
2019-02-18 00:41:21 -0500

    added map's header, laid intial styling groundwork

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Image,
   Platform,
@@ -6,21 +6,48 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
-  View,
-} from 'react-native';
-import { WebBrowser } from 'expo';
-import Map from "../components/Map"
+  View
+} from "react-native";
+import { WebBrowser } from "expo";
+import Map from "../components/Map";
+import styled from "styled-components";
 
-import { MonoText } from '../components/StyledText';
+import BlueText from "../assets/images/BlueLogo.png";
+
+const MommaDiv = styled.View``;
+
+const Header = styled.View`
+  height: 10%;
+  background-color: #38b6ff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const MapDiv = styled.View`
+  height: 100%;
+`;
+
+const LogoText = styled.Image`
+  height: 40px;
+  width: 190px;
+  margin-top: 12px;
+`;
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
-    header: null,
+    header: null
   };
 
   render() {
     return (
-      <Map />
+      <MommaDiv>
+        <Header>
+          <LogoText source={BlueText} />
+        </Header>
+        <MapDiv>
+          <Map />
+        </MapDiv>
+      </MommaDiv>
     );
   }
 }
